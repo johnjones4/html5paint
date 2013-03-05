e2ad0ed0bf1fcd687ab317effec9d5a80dbd3e74
John Jones <johnjones4@gmail.com>
2013-03-04 20:57:20 -0500

    Changed port

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app  = express();
 app.use(express.static(__dirname + '/static'));
 
-app.listen(process.env.VMC_APP_PORT || 1337, function() {
+app.listen(1228, function() {
    console.log("Server started");
 });
